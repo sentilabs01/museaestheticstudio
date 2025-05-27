22761de38c3c5571b7e76236ebd75d87047c767f
sentilabs01 <sentilabs01@gmail.com>
2025-05-27 13:49:19 -0700

    fix: use relative imports for Button for AWS compatibility

--- components/sections/Pricing.tsx
+++ components/sections/Pricing.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { useState } from 'react'
-import { Button } from "@/components/ui/button"
+import { Button } from "../ui/button"
 import { PricingCard } from '../ui/dark-gradient-pricing'
 import { PricingSection } from './PricingSection'
 
